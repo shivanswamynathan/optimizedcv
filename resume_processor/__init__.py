ac17aeeba5fcf0592d9209702898612166a109ef
shivanswamynathan <46991694+shivanswamynathan@users.noreply.github.com>
2025-05-13 18:19:59 +0530

    Delete resume_processor directory

--- resume_processor/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = 'resume_processor.apps.ResumeProcessorConfig'
--- resume_processor/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class ResumeProcessorConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'resume_processor'
